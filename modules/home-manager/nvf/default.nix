4a20f4d517871153abf2d6d3d3c877f095155b2b
MatthieuSacquet <matthieu.sacquet@epitech.eu>
2026-05-30 16:12:15 +0200

    feat: adding nvf

---
+++ modules/home-manager/nvf/default.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, inputs, ...}:
+
+{
+
+import [ inputs.nvf.nixosModules.default ];
+
+  programs.nvf = {
+    enable = true;
+    # Your settings need to go into the settings attribute set
+    # most settings are documented in the appendix
+    settings = {
+      vim.viAlias = true;
+      vim.vimAlias = true;
+      vim.lsp = {
+        enable = true;
+      };
+    };
+  };
+}
